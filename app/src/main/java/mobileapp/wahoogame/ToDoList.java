c4b96489e03eca3e860d02b1b467536ca5788362
Stephen Nickle <stephen.nickle@gmail.com>
2015-09-16 08:51:06 -0500

    This is the completed Alpha version. It does not contain AI or multiplayer across multiple devices. It does not contain Cameron's updated images. This is a very simple initial release to get Wahoo onto the store.

--- app/src/main/java/mobileapp/wahoogame/ToDoList.java
+++ app/src/main/java/mobileapp/wahoogame/ToDoList.java
@@ -1,27 +1,11 @@
 /*
+Organize
 
-DONE - Change popUp to appear at gameOver
+Revert to original images
 
-WAIT - Figure out how to add to Android Store
+Make sure functionality is working properly
 
-Clean up menus
+Add images for all sizes
 
-DONE - Add about information
-
-DONE - (Not going to Exit - Main Menu instead)Exit for popUp
-
-DONE - (No exit button in Android)Exit from main menu
-
-Make images scalable
-
-DONE - Dice Animation
-
-DONE - Add winner to Dialog
-
-DONE??? - Fix status TextView - Looks done to me, not sure what is wrong with it.
-
-DONE - Update documentation
-
-Revert back to original dice image after players turn is completed
-
- */
+Add to app store
+*/
